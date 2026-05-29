338eeae8ce61d4817245cbc9cbbe8f1e0ee0e77d
mringler <m@mringler.net>
2026-05-29 16:22:15 +0200

    make codecov happy

--- tests/Propel/Tests/Runtime/PerplTest.php
+++ tests/Propel/Tests/Runtime/PerplTest.php
@@ -13,6 +13,7 @@ use Propel\Runtime\ServiceContainer\ServiceContainerInterface;
 use Propel\Runtime\ServiceContainer\StandardServiceContainer;
 use Propel\Tests\Helpers\BaseTestCase;
 use Propel\Runtime\Exception\PropelException;
+use Psr\Log\LoggerInterface;
 
 class PerplTest extends BaseTestCase
 {
@@ -71,5 +72,79 @@ class PerplTest extends BaseTestCase
         $this->expectException(PropelException::class);
         Perpl::getStandardServiceContainer();
     }
-    
+
+    public function testGetServiceContainerSetsInstance(): void
+    {
+        $this->setObjectPropertyValue(null, 'serviceContainer', null, Perpl::class);
+        Perpl::getServiceContainer();
+        $sc = $this->getObjectPropertyValue(Perpl::class, 'serviceContainer');
+        $this->assertInstanceOf(StandardServiceContainer::class, $sc);
+    }
+
+    /**
+     * @return array<array{string, ?array}>
+     */
+    public static function ServiceContainerFacadedMethodsProvider(): array
+    {
+        return [
+            ['getDatabaseMap'],
+            ['getAdapter'],
+            ['getDefaultDatasource'],
+            ['getConnectionManager', ['foo']],
+            ['closeConnections'],
+            ['getConnection'],
+            ['getWriteConnection', ['foo']],
+            ['getReadConnection', ['foo']],
+            ['getProfiler'],
+            ['getLogger'],
+        ];
+    }
+
+    #[\PHPUnit\Framework\Attributes\DataProvider('ServiceContainerFacadedMethodsProvider')]
+    public function testGetDatabaseMap(string $facedMethodName, array $args = []): void
+    {
+        $sc = $this->createMock(ServiceContainerInterface::class);
+        $sc->expects($this->once())
+            ->method($facedMethodName);
+        Perpl::setServiceContainer($sc);
+        Perpl::$facedMethodName(...$args);
+    }
+
+    /**
+     * @return array<array{int, string}>
+     */
+    public static function LogLevelProvider(): array
+    {
+        return [
+            [Perpl::LOG_ALERT, 'alert'],
+            [Perpl::LOG_CRIT, 'critical'],
+            [Perpl::LOG_DEBUG, 'debug'],
+            [Perpl::LOG_EMERG, 'emergency'],
+            [Perpl::LOG_ERR, 'error'],
+            [Perpl::LOG_INFO, 'info'],
+            [Perpl::LOG_NOTICE, 'notice'],
+            [Perpl::LOG_WARNING, 'warning'],
+        ];
+    }
+
+    #[\PHPUnit\Framework\Attributes\DataProvider('LogLevelProvider')]
+    public function testLogLevels(int $logLevel, string $loggerMethodName): void
+    {
+        $logger = $this->createMock(LoggerInterface::class);
+        $logger->expects($this->once())
+            ->method($loggerMethodName);
+
+        $sc = $this->createMock(ServiceContainerInterface::class);
+        $sc->expects($this->once())
+            ->method('getLogger')->willReturn($logger);
+
+        Perpl::setServiceContainer($sc);
+        Perpl::log('', $logLevel);
+    }
+
+    public function testClassAlias(): void
+    {
+        $this->assertTrue(class_exists('\Propel\Runtime\Propel'));
+        $this->assertInstanceOf(Perpl::class, new \Propel\Runtime\Propel());
+    }
 }
